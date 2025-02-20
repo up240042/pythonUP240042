9b564e7a492613dcde0b726ad08e7cb4d70db54b
Luis <up240042@alumnos.upa.edu.mx>
2025-02-20 07:49:46 -0600

    Avance 2.1

--- trabajos/ejercicos.py
+++ trabajos/ejercicos.py
@@ -21,15 +21,34 @@ perimeter = lado1 + lado2 + lado3
 print("El perimetro del triangulo es de: ", perimeter)
 
 ##6 Length and Width
-Length = int(input("Ingresa el valor del ancho: ")) ##Largo
+print("")
+Length = int(input("Ingresa el valor del largo: ")) ##Largo
 Width = int(input("Ingresa el valor del ancho: ")) #Ancho
 area1 = Length * Width
 perimeter1 = 2 * (Length + Width)
+print("El perimetro es de: ", perimeter1)
 
 ##7 Radius
+print("")
 radius = float(input("Ingresa la circunferencia de tu circulo: "))
 pi = float(3.1416)
-areac = float(pi * 2(radius))
+areac = float(pi * radius * radius)
 circunference = float(2 * pi * radius)
-print("Tu area es de: ", radius)
+print("Tu area es de: ", areac)
 print("Tu circunferencia es de: ", circunference)
+
+##8 Slope
+print("")
+print("Interaccion de la pendiente:")
+pendiente = 2
+intY = -2
+intX = intY
+pen = ((pendiente * intX) + intY)
+print("La interacción es de: ", pen) 
+
+##9 Euclidean distance
+print("")
+x = (2,2)
+y = (6,10)
+m = y/x
+print(m)
