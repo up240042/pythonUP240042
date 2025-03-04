e1bb1d22a2e3c9636b58aebb605915e1b97a4cd2
Luis <up240042@alumnos.upa.edu.mx>
2025-03-04 09:38:35 -0600

    Avance 2

--- trabajos/Dia 4.py
+++ trabajos/Dia 4.py
@@ -31,7 +31,7 @@ print(len(com2))
 
 ## 6
 print("")
-may = com2.upper()
+may = com2.upper() ##Convierte minisculas en mayusculas
 print(may)
 
 ## 7
@@ -60,3 +60,38 @@ sub = "For"
 print(hol.index(sub))
 print()
 
+## 11 
+print("")
+print("Ejercicio 11")
+cod = "Coding For All"
+rem = cod.replace("Coding", "Hola") ##Poner la palabra a remplazar y la nueva
+print("Se Remplazo la palabra Coding," , rem)
+
+## 12 
+print("")
+print("Ejercicio 12")
+cod1 = "Python for Everyone"
+print(cod1)
+rem = cod1.replace("Everyone" , "All")
+print("Palabra nueva:  ", rem)
+
+## 13
+print("")
+print("Ejercicio 13")
+se = rem.split() ##Divide la palabra
+print(se)
+
+## 14
+print("")
+print("Ejercicio 14")
+app = ("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon")
+se1 = app.split(",") # Separa usando la coma seguida de un espacio
+print(se1)
+
+## 15 
+print("")
+print("Ejercicio 15")
+co = "Coding For All"
+#se2 = co.split()
+#se2 = sduir aqui
+print()
